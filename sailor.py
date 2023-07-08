7420a608eb5747bfa0e4aa5648a6913313e4a10e
Hogchops <31632064+Hogchops@users.noreply.github.com>
2023-07-08 17:25:34 -0400

    Create sailor.py

---
+++ sailor.py
@@ -0,0 +1,37 @@
+"""
+Some nice comments
+
+@author:
+@version:
+"""
+
+import random
+
+def main():
+    # Interesting code
+    
+    
+def rand_step():
+    '''
+    Generates the direction that the pirate steps.
+
+    Returns
+    -------
+    int
+        1 for right, -1 for left.
+
+    '''
+    return 1 if random.randint(0, 1) else -1
+
+
+def display_board():
+    # Interesting code
+    
+
+def output_experiment_stats():
+    # Interesting code
+    
+def drunken_walk():
+    #Interesting code
+    
+main()
